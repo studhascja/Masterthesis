212908b65c113ed5cdbe914356aa5b9edcc046a6
Jakob Hasche <jockhasche@gmail.com>
2025-03-31 14:27:05 +0200

    Added Cleanup to config script

--- code/config-script.sh
+++ code/config-script.sh
@@ -4,20 +4,38 @@ CONFIG_FILE="/etc/dnsmasq.conf"
 INTERFACE="wlp1s0"
 DHCP_RANGE="dhcp-range=192.168.1.10,192.168.1.100,12h"
 
-# Configure DHCP server
+# Configure DHCP-Server
 grep -q "^interface=$INTERFACE" "$CONFIG_FILE" || echo "interface=$INTERFACE" | sudo tee -a "$CONFIG_FILE"
 grep -q "^$DHCP_RANGE" "$CONFIG_FILE" || echo "$DHCP_RANGE" | sudo tee -a "$CONFIG_FILE"
+echo "Configured DHCP-server"
 
-# Configure network-interface
+# Configure network interface
 sudo ifconfig "$INTERFACE" 192.168.1.1 netmask 255.255.255.0 up
+echo "Configure Interface"
 
-# Check if sestemd-resolve is running and stop it, if so
+# Stop systemd-resolved (if active), to clear port 53
 if systemctl is-active --quiet systemd-resolved; then
     sudo systemctl stop systemd-resolved
+    echo "Stopped systemd-resolved."
 fi
 
-# start Dnsmasq
+# start Dnsmasq 
 sudo systemctl restart dnsmasq
+echo "started Dnsmasq."
 
-# start Hostapd 
+# start Hostapd
 sudo hostapd /etc/hostapd/hostapd.conf
+
+
+# Cleanup
+echo "End script"
+
+# Stop dnsmasq
+sudo systemctl stop dnsmasq
+echo "Stopped Dnsmasq"
+
+# Start systemd-resolved
+sudo systemctl start systemd-resolved
+echo "Started systemd-resolved"
+
+exit 0
