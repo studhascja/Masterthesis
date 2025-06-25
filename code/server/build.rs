0bc48a119c20a2703a85806675241fed29444209
Jakob Hasche <jakob.hasche@stud.th-rosenheim.de>
2025-06-25 14:05:24 +0200

    created build.rs for yocto

---
+++ code/server/build.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    let kernel_headers = env::var("KERNEL_HEADERS").unwrap_or("/usr/src/kernel".to_string());
+    SkeletonBuilder::new()
+        .source("src/bpf/monitore.bpf.c")
+        .clang_args(&format!("-I{}/arch/arm64/include/generated/uapi -I{}/include -I{}/include/uapi -I{}/include/generated", kernel_headers, kernel_headers, kernel_headers, kernel_headers))
+        .build_and_generate("src/bpf/monitore.skel.rs")
+        .unwrap();
+}
